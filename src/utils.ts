d1f502ce22488e79e62f7529460b836bcfeded0e
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-10-31 03:36:11 +0100

    :sparkles: Querystring parsing utilities

---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+import { Queries } from './types'
+
+export const parseQuerystring = (querystring: string): Queries => {
+    const splitUrl = querystring.split('?')
+
+    return splitUrl[splitUrl.length - 1]
+        .split('&')
+        .map(pair => pair.split(''))
+        .map(([key, value]) => ({ key, value }))
+}
+
+export const stringifyQuerystring = (queries: Queries): string =>
+    queries.map(({ key, value }) => `${key}=${value}`).reduce((queryString, current) => `${queryString}&${current}`, '')
