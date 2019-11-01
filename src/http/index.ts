c577fb68ff9c9392fd3971de521074040efe5221
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-11-01 06:21:03 +0100

    :construction: Improved sender function

--- src/http/index.ts
+++ src/http/index.ts
@@ -3,25 +3,43 @@ import fetch from 'cross-fetch'
 import { buildResponse } from '../data'
 import { Request, Response } from '../types'
 import { ResponseError } from './response-error'
+import { stringifyQuerystring } from '../utils'
 
-export const send = async (request: Request): Promise<Response> =>
-    fetch(`${request.host}${request.path}`, {
+export const send = async (request: Request): Promise<Response> => {
+    const host = request.host
+    const path = `${request.path}${request.query ? '?' + stringifyQuerystring(request.query) : ''}`
+
+    const bodyString = request.body
+        ? typeof request.body === 'object'
+            ? JSON.stringify(request.body)
+            : request.body
+        : undefined
+
+    return fetch(`${host}${path}`, {
         method: request.method,
         headers: request.headers,
+        body: bodyString,
     })
         .then(response => checkError(response))
         .then(response => {
             const headers = response.headers
+            const body = response.body
 
             return buildResponse({
+                status: response.status,
+
                 host: request.host,
                 path: request.path,
+
                 headers,
-                status: response.status,
+                body,
             })
         })
+}
 
 const checkError = (response: any) => {
+    // TODO Figure out typings
+
     if (response.status >= 400) {
         throw new ResponseError('HTTP status code in error range', response)
     }
