5d2b66bdd001b072a7444b242df7eeea9fb3e6aa
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-11-06 14:07:17 +0100

    :construction: Client test

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,12 +1,53 @@
 import { Aika } from '../src'
 
+// http://jsonplaceholder.typicode.com/guide.html
+
 describe('Test client', () => {
     let aika: Aika
 
     beforeAll(() => {
-        // TODO Make own testing host
-        aika = new Aika('https://cat-fact.herokuapp.com')
+        aika = new Aika('https://jsonplaceholder.typicode.com')
     })
 
-    test.todo('GET Request')
+    test('GET Request', async () => {
+        const expected = {
+            userId: 1,
+            id: 1,
+            title: 'delectus aut autem',
+            completed: false,
+        }
+
+        try {
+            const response = await aika.get('/todos/1')
+
+            expect(response.json).toEqual(expected)
+        } catch (err) {
+            fail(err)
+        }
+    })
+
+    test('POST Request', async () => {
+        const expected = {
+            id: 101,
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        }
+
+        try {
+            const response = await aika.post(
+                '/posts',
+                {},
+                {
+                    title: 'foo',
+                    body: 'bar',
+                    userId: 1,
+                },
+            )
+
+            expect(response.json).toEqual(expected)
+        } catch (err) {
+            fail(err)
+        }
+    })
 })
